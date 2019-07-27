9f404334b03192eb1911fa170d3238bed2bf27cd
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-27 00:11:25 -0700

    mongo schema example

---
+++ server/db/schemaMongo.example.js
@@ -0,0 +1,31 @@
+const restaurants = {
+  {
+    id: 0,
+    name: '',
+    keywords: ['',''],
+    reviews: [
+      {
+        user: {
+          name: '',
+          location: '',
+        },
+        reviewText: '',
+        foodRating: 0,
+        serviceRating: 0,
+        ambienceRating: 0,
+        valueRating: 0,
+        noise: 0,
+        helpfulCount: 0,
+        reviewDate: '9/1/10 7:00 pm',
+        recommended: true,
+      }
+    ],
+    avgOverall: 0,
+    avgFoodRating: 0,
+    avgValRating: 0,
+    avgServiceRating: 0,
+    avgNoise: 0,
+    avgFoodRating: 0,
+    avgRecommend: 0,
+  }
+}
