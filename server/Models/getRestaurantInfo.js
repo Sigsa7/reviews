8c912e1b4077b62e3fcef23aa413afb553941fa8
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-29 19:39:58 -0700

    updated with star filter query

--- server/Models/getRestaurantInfo.js
+++ server/Models/getRestaurantInfo.js
@@ -3,34 +3,50 @@ const pg = require('../db');
 const getRestaurantInfo = (req, res) => {
   const { restaurantId } = req.params;
 
-  const { sort } = req.body;
+  const { sort, keyword, star } = req.body;
   let orderByStr = '';
-  let selectStr;
-  let sortDirection = 'DESC';
+  let selectStr = orderByStr;
+  let sortDirection = '';
+  let keywordStr = '';
+  let starStr = '';
 
   if (sort === 'highest rating') {
     orderByStr = 'CAST((reviews.foodrating+reviews.servicerating+reviews.ambiencerating+reviews.valuerating) AS FLOAT)/4';
+    sortDirection = ' DESC,';
   }
 
   if (sort === 'lowest rating') {
     orderByStr = 'CAST((reviews.foodrating+reviews.servicerating+reviews.ambiencerating+reviews.valuerating) AS FLOAT)/4';
-    sortDirection = 'ASC'
+    sortDirection = ' ASC,';
+  }
+
+  if (keyword !== undefined) {
+    for (let i = 0; i < keyword.length; i ++) {
+      keywordStr += `AND reviews.reviewText LIKE '%${keyword[i]}%' `;
+    }
+
+    keywordStr = keywordStr.slice(0, -1);
   }
 
   if (orderByStr !== '') {
-    selectStr = orderByStr + ' AS avg_overal_from_reviews';
-    console.log(selectStr)
+    selectStr = ', ' + orderByStr + ' AS avg_overal_from_reviews';
+  }
+
+  if (star !== '') {
+    starStr = `AND CAST((reviews.foodrating+reviews.servicerating+reviews.ambiencerating+reviews.valuerating) AS FLOAT)/4 >= ${star} AND CAST((reviews.foodrating+reviews.servicerating+reviews.ambiencerating+reviews.valuerating) AS FLOAT)/4 < ${star + 1}`;
   }
 
   const query = {
-    name: 'get-restaurant-info',
     text: `
-      SELECT *, ${selectStr} FROM restaurants
+      SELECT *${selectStr} FROM restaurants
         INNER JOIN reviews
           ON restaurants.id=reviews.restaurantid
-        WHERE restaurants.id=$1
+        WHERE
+          restaurants.id=$1
+          ${keywordStr}
+          ${starStr}
         ORDER BY
-          ${orderByStr} ${sortDirection},
+          ${orderByStr} ${sortDirection}
           reviews.reviewdate DESC
         LIMIT 10;
     `,
