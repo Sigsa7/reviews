1eb30892ce9d8307d55c19834a18cba5ff937691
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-29 20:04:12 -0700

    postgres sample shell query

--- server/Models/postgresQuery.sql
+++ server/Models/postgresQuery.sql
@@ -5,13 +5,6 @@ INSERT INTO reviews (restaurantId, userId, foodRating, serviceRating, ambienceRa
   (1, 1, 1, 2, 3, 4, 1564195654, 4, false, 'adaredugjbw awsidujvmvw. weioknwef asdicowlefj.')
 RETURNING id;
 
--- GET THE REIVEWS FOR A RESTAURANT BASED ON RESTAURANT ID SORT BY POST DATE
-
-EXPLAIN ANALYZE
-SELECT * FROM reviews
-  WHERE restaurantId=893949
-  ORDER BY reviewDate DESC;
-
 -- GET THE INFO AND REVIEWS FOR A RESTAURANT SORT BY POST DATE
 
 EXPLAIN ANALYZE
@@ -20,6 +13,22 @@ SELECT * FROM restaurants
     ON restaurants.id=reviews.restaurantid
   WHERE restaurants.id=9999
   ORDER BY reviews.reviewdate DESC;
+
+-- GET THE INFO AND REVIEWS FOR A RESTAURANT WITH OTHER SORTING CRITERIAS
+SELECT *, CAST((reviews.foodrating+reviews.servicerating+reviews.ambiencerating+reviews.valuerating) AS FLOAT)/4 AS avg_overal_from_reviews
+FROM restaurants
+        INNER JOIN reviews
+          ON restaurants.id=reviews.restaurantid
+        WHERE restaurants.id=999999
+        ORDER BY
+CAST((reviews.foodrating+reviews.servicerating+reviews.ambiencerating+reviews.valuerating) AS FLOAT)/4 ASC, reviews.reviewdate DESC;
+
+-- GET THE REIVEWS FOR A RESTAURANT BASED ON RESTAURANT ID SORT BY POST DATE
+
+EXPLAIN ANALYZE
+SELECT * FROM reviews
+  WHERE restaurantId=893949
+  ORDER BY reviewDate DESC;
 
 -- UPDATE A REVIEW'S HELPFUL COUNT
 EXPLAIN ANALYZE
