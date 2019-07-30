0e1dd90b53b6ddf81c5cca68a2fe5d193c5a52a2
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-29 19:41:39 -0700

    CRUD API routing defined

--- server/Router.js
+++ server/Router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const createReviews = require('./Models/createReviews');
+const getRestaurantInfo = require('./Models/getRestaurantInfo');
 
 const router = express.Router();
 
 router.post('/:restaurantId/reviews', createReviews);
-
+router.get('/:restaurantId/reviews', getRestaurantInfo);
+router.patch('/:restaurantId/:reviewId', updateReview);
+router.delete('/:restaurantId/:reviewId', deleteReview);
 
 module.exports = router;
