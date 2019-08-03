5a06c0f70a194ea887abce72abde26dc02aa9b7c
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-03 10:21:27 -0700

    split get requests to two routes

--- server/Router.js
+++ server/Router.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const createReviews = require('./Models/createReviews');
 const getRestaurantInfo = require('./Models/getRestaurantInfo');
+const getRestaurantReviewLisitng = require('./Models/getRestaurantReviewLisitng');
 const updateReview = require('./Models/updateReview');
 const deleteReview = require('./Models/deleteReview');
 
 const router = express.Router();
 
 router.post('/:restaurantId/reviews', createReviews);
-router.get('/:restaurantId/reviews', getRestaurantInfo);
-router.patch('/:restaurantId/:reviewId', updateReview);
-router.delete('/:restaurantId/:reviewId', deleteReview);
+router.get('/:restaurantId/reviews/reviewListing', getRestaurantReviewLisitng);
+router.get('/:restaurantId/reviews/restaurantInfo', getRestaurantInfo);
+router.patch('/:restaurantId/reviews/:reviewId', updateReview);
+router.delete('/:restaurantId/reviews/:reviewId', deleteReview);
 
 module.exports = router;
