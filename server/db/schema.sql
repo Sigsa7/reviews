48b7f790c2b614e1a248b4dc78da13200426bf66
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-03 10:21:58 -0700

    updated postgres schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,26 +1,26 @@
 DROP TABLE reviews;
-DROP TABLE restaurants;
-DROP TABLE users;
+-- DROP TABLE restaurants;
+-- DROP TABLE users;
 
-CREATE TABLE IF NOT EXISTS restaurants (
-  id bigserial PRIMARY KEY,
-  restaurantName varchar(100) NOT NULL,
-  neighborhood varchar(100) NOT NULL,
-  keywords text[],
-  avgOverall real,
-  avgFood real,
-  avgService real,
-  avgAmbience real,
-  avgValue real,
-  avgNoise real,
-  avgRecommend real
-);
+-- CREATE TABLE IF NOT EXISTS restaurants (
+--   id bigserial PRIMARY KEY,
+--   restaurantName varchar(100) NOT NULL,
+--   neighborhood varchar(100) NOT NULL,
+--   keywords text[],
+--   avgOverall real,
+--   avgFood real,
+--   avgService real,
+--   avgAmbience real,
+--   avgValue real,
+--   avgNoise real,
+--   avgRecommend real
+-- );
 
-CREATE TABLE IF NOT EXISTS users (
-  id bigserial PRIMARY KEY,
-  userName varchar(100) NOT NULL,
-  userLocation varchar(100) NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS users (
+--   id bigserial PRIMARY KEY,
+--   userName varchar(100) NOT NULL,
+--   userLocation varchar(100) NOT NULL
+-- );
 
 CREATE TABLE IF NOT EXISTS reviews (
   id bigserial NOT NULL PRIMARY KEY,
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS reviews (
   serviceRating smallint NOT NULL,
   ambienceRating smallint NOT NULL,
   valueRating smallint NOT NULL,
+  avgRating real NOT NULL,
   helpfulCount smallint DEFAULT 0,
   reviewDate int,
   noise smallint NOT NULL,
@@ -37,11 +38,11 @@ CREATE TABLE IF NOT EXISTS reviews (
   reviewText varchar(1000) NOT NULL
 );
 
-COPY users(userName, userLocation) from '/Users/jenniezeng/Documents/HackReactor/Course/SDC/reviews/server/db/postgres_user.csv' DELIMITER ',' CSV HEADER;
+-- COPY users(userName, userLocation) from '/Users/jenniezeng/Documents/HackReactor/Course/SDC/reviews/server/db/postgres_user.csv' DELIMITER ',' CSV HEADER;
 
-COPY restaurants(restaurantName, neighborhood, keywords,avgOverall,avgFood,avgService,avgAmbience,avgValue,avgNoise,avgRecommend) from '/Users/jenniezeng/Documents/HackReactor/Course/SDC/reviews/server/db/postgres_restaurants.csv' DELIMITER ',' CSV HEADER;
+-- COPY restaurants(restaurantName, neighborhood, keywords,avgOverall,avgFood,avgService,avgAmbience,avgValue,avgNoise,avgRecommend) from '/Users/jenniezeng/Documents/HackReactor/Course/SDC/reviews/server/db/postgres_restaurants.csv' DELIMITER ',' CSV HEADER;
 
-COPY reviews(restaurantid,userid,foodrating,servicerating,ambiencerating,valuerating,noise,recommended,reviewdate,reviewtext) from '/Users/jenniezeng/Documents/HackReactor/Course/SDC/reviews/server/db/postgres_reviews.csv' DELIMITER ',' CSV HEADER;
+COPY reviews(restaurantid,userid,foodrating,servicerating,ambiencerating,valuerating,avgrating,noise,recommended,reviewdate,reviewtext) from '/Users/jenniezeng/Documents/HackReactor/Course/SDC/reviews/server/db/postgres_reviews.csv' DELIMITER ',' CSV HEADER;
 
 CREATE index idx_restaurant_id ON reviews(restaurantId);
 CREATE index idx_user_id ON reviews(userId);
