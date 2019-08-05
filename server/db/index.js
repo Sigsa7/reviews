c9feed4aeb53f07a7093da81cb919412591a0f12
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-05 12:30:37 -0700

    psql connection update

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const connectionStr = {
-  host: 'http://13.57.3.155', // server name or IP address;
+  host: '13.57.3.155', // server name or IP address;
   // port: 5432,
   database: 'sigsa7_reviews',
   user: 'ec2-user',
