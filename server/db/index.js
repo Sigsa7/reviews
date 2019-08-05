571e28dbea7b0b305c6a854902038c4f7d613a1d
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-05 11:41:37 -0700

    updated postgres conneciton for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const connectionStr = {
-  host: 'localhost', // server name or IP address;
+  host: 'ec2-13-57-3-155.us-west-1.compute.amazonaws.com', // server name or IP address;
   port: 5432,
   database: 'sigsa7_reviews',
-  user: 'jenniezeng',
+  user: 'ec2-user',
   password: '',
 };
 
