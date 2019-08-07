bd60c984eb90900b05e397daf897353f5582bd3e
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-07 15:30:53 -0700

    loaderio csv

---
+++ generatePayload.js
@@ -0,0 +1,39 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const writeFile = fs.createWriteStream('./generatePayload.json');
+
+let i = 7000;
+const sortOptions = ['highest rating', 'lowest rating', ''];
+const keys = ['sort', 'keywords', 'star'];
+const values = [];
+
+let keywords;
+let sort;
+let star;
+let randomKeywordCount;
+
+while (i > 0) {
+  let keywords = [];
+  sort = sortOptions[Math.floor(Math.random() * 3)];
+  star = Math.floor(Math.random() * 6);
+
+  randomKeywordCount = Math.floor(Math.random() * 4);
+
+  while (randomKeywordCount > 0) {
+    keywords.push(faker.lorem.word());
+    randomKeywordCount --;
+  }
+
+  values.push(JSON.stringify([sort, keywords, star]));
+
+  i --;
+}
+
+const body = {};
+body.keys = keys;
+body.values = values;
+
+writeFile.write(JSON.stringify(body));
+
+writeFile.end()
--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.get('/loaderio-bc5ce11e94267dcc5ea0be046c5b74a0', (req, res) => {
   res.status(200).send('loaderio-bc5ce11e94267dcc5ea0be046c5b74a0');
 });
 
+app.get('/payload', (req, res) => {
+  res.status(200).sendFile('../generatePayload.json');
+});
+
 app.use('/', expressStaticGzip(path.join(__dirname, '../client/dist'), {
   enableBrotli: true,
   orderPreference: ['br', 'gz']
